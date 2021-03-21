257d636b25d2f38a14500b3c1b09276dfbf0d4d6
Pedro Tristão <80562516+pedro-tristao@users.noreply.github.com>
2021-03-21 13:53:36 -0300

    test

--- src/js/components/app-component/App.js
+++ src/js/components/app-component/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          monstra
+          teste
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Que teste teste teste
+         teste
         </a>
       </header>
     </div>
